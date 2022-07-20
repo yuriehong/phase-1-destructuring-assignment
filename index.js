2bbeeee8b7661b9281fbf103d5f2d632c00b4342
+ <+>
2022-07-19 21:31:55 -0400

    commit tree 4ab94ec1f43d45261ce4bd7f70fb4aded6742bd6

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+const [blackAndWhite, , black, pink, ] = farmAnimals.split(' ');
+
+
+const[red, orange, yellow, green, blue, indigo, violet] = colors;
+const[r,o,y,g,b,i,v] = colors;
+const[ , , , , , indg, ] = colors;
+
+const {muppetName, color, song, job, partner} = muppet;
+const {song2, song4} = nestedMuppet.album.theMuppetMovie;
+const {nestedJob, nestedPartner} = nestedMuppet;
+
 
 // Strings
 
